24aa0c0e770f07dca14b94712274ace306edbb27
sahil-sheikh-17 <63206410+sahil-sheikh-17@users.noreply.github.com>
2022-09-10 01:27:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,8 @@ terraform {
 }
 
 provider "aws" {
-  shared_credentials_file = "$HOME/.aws/credentials"
+  access_key              = "AKIATGPOXLRZGWD3JA7A"
+  secret_key              = "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"  
   profile                 = "default"
   region                  = "us-east-1"
 }
